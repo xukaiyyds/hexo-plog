9e5bfa9a45e3b0c5a6b4b9324ce31b8246ee2abe
xukaiyyds <3353212714@qq.com>
2024-03-01 15:23:48 +0800

    Site updated: 2024-03-01 15:23:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d87f7e0c/","posts/b15be43a/","posts/7ddf6b3a/"];function toRandomPost(){
+var posts=["posts/5126acfb/","posts/4ae524d/","posts/7ddf6b3a/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
