d738e734740396a33e479de998dfcccc25d3d0ed
xukaiyyds <3353212714@qq.com>
2025-12-01 17:02:12 +0000

    update hexo-plog Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5126acfb/","posts/502f1247/","posts/63f29015/","posts/4ae524d/","posts/7a6cbde0/","posts/b15be43a/","posts/324981b4/","posts/7ddf6b3a/"];function toRandomPost(){
+var posts=["posts/5126acfb/","posts/63f29015/","posts/502f1247/","posts/4ae524d/","posts/7a6cbde0/","posts/b15be43a/","posts/324981b4/","posts/7ddf6b3a/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
